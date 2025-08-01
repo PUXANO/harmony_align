f61e9284107eeee3c83067444c8691b05230a9de
Jos <jvdoorss@gmail.com>
2025-08-01 12:03:22 +0200

    rename variables more semantically

--- src/align/volume_heat_kernel.py
+++ src/align/volume_heat_kernel.py
@@ -1,35 +1,46 @@
 '''
+Module to decompose a density into its moments in a spherical-bessel expansion and compare them to the corresponding moments of a Gaussian blur of discrete (atom) positions.
 
+The main class is `Registrator`: the problem at hand is often referred to as `3D registration`. The moments from the volume are referred to as `Slm` (spherical harmonics) and the moments from the coordinates as `Vlm` (volume moments). The `Registrator` class can be used to preprocess a volume, filter the relevant modes, and align a set of coordinates to the volume by finding the best rotation.
+
+(c) 2025 Puxano BV, licensed under the Apache License 2.0.
 '''
 
 
 from typing import Generator, Self
-from time import time
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import numpy as np
-from scipy.spatial.transform import Rotation
 import pandas as pd
 
 from tools.wigner import WignerGallery
 from tools.utils import Grid, jn_zeros
 from tools.sequences import SphericalSequence
 
-class Spatial(SphericalSequence, Grid):
+class SphericalGrid(SphericalSequence, Grid):
+    '''
+    Grid allowing decomposition in spherical harmonics and spherical Bessel functions.
+    '''
     def __init__(self, grid_size = 100, scale = 1.0, l_max: int = 10):
         SphericalSequence.__init__(self, l_max)
         bound = (grid_size - 1) / 2 * scale
         ticks = np.linspace(-bound,bound,grid_size)
         Grid.__init__(self,*np.meshgrid(ticks,ticks,ticks, indexing='ij'))
 
-    def Slm(self,voxels: np.ndarray, k_profile: np.ndarray) -> Generator[np.ndarray,None,None]:
-        '''Eigenfunctions for the volume'''
-        for l,m in self.lm():
-            yield np.sum(self.fourier_bessel_expansion(l,m,k_profile[l]) * voxels[...,None] * self.dV,axis=(0,1,2))
+    def voxel_moment(self, l: int, m: int, voxels: np.ndarray, k_profile_l: np.ndarray) -> np.ndarray:
+        '''
+        Compute the spherical harmonic moment for the given voxel grid.
+        '''
+        return np.sum(self.fourier_bessel_expansion(l,m,k_profile_l) * voxels[...,None] * self.dV,axis=(0,1,2))
 
-    def Sl(self,voxels: np.ndarray, k_profile: np.ndarray) -> Generator[np.ndarray,None,None]:
+    def voxel_moments_lm(self,voxels: np.ndarray, k_profile: np.ndarray) -> Generator[np.ndarray,None,None]:
+        '''Moments for the given voxel distribution over this Grid.'''
+        for l,m in self.lm():
+            yield self.voxel_moment(l,m,voxels,k_profile[l])
+
+    def voxel_moments_l(self,voxels: np.ndarray, k_profile: np.ndarray) -> Generator[np.ndarray,None,None]:
         multiplet = []
-        for (l,m),Slm in zip(self.lm(),self.Slm(voxels, k_profile)):
+        for (l,m),Slm in zip(self.lm(),self.voxel_moments_lm(voxels, k_profile)):
             multiplet.append(Slm)
             if l == m:
                 yield np.stack(multiplet)
@@ -42,29 +53,43 @@ class Spatial(SphericalSequence, Grid):
             res += np.exp(-0.5 / sigma**2 * np.sum((self.grid - atom)**2,-1))
         return res
     
-    def Vlm(self, coordinates: np.ndarray, sigma: float, k_profile: np.ndarray, k_density: np.ndarray = None) -> Generator[np.ndarray,None,None]:
+    def coordinate_moments_lm(self, coordinates: np.ndarray, sigma: float, k_profile: np.ndarray, k_density: np.ndarray = None) -> Generator[np.ndarray,None,None]:
+        '''Generate all moments from the coordinates in sequence-order, for each l and m.'''
         coordinates_as_grid = Grid(*coordinates.T)
         for l,m in self.lm():
-            # TODO precompute k-density
-            density = np.exp(- self.k[l] **2 * sigma**2 / 2) if k_density is None else k_density[l]
+            density = np.exp(- k_profile[l] **2 * sigma**2 / 2) if k_density is None else k_density[l]
             yield np.sum(coordinates_as_grid.fourier_bessel_expansion(l,m,k_profile[l]) * density,axis=0) #should be 1D
 
-    def Vl(self, coordinates: np.ndarray, sigma: float, k_profile: np.ndarray, k_density: np.ndarray = None) -> Generator[np.ndarray,None,None]:
+    def coordinate_moments_l(self, coordinates: np.ndarray, sigma: float, k_profile: np.ndarray, k_density: np.ndarray = None) -> Generator[np.ndarray,None,None]:
         multiplet = []
-        for (l,m),Vlm in zip(self.lm(),self.Vlm(coordinates,sigma, k_profile, k_density)):
+        for (l,m),Vlm in zip(self.lm(),self.coordinate_moments_lm(coordinates,sigma, k_profile, k_density)):
             multiplet.append(Vlm)
             if l == m:
                 yield np.array(multiplet)
                 multiplet = []
 
-    def l(self) -> Generator[int,None,None]:
-        '''Generate l for the spherical harmonics'''
-        for l in range(self.l_max + 1):
-            yield l
-
-class Registrator(Spatial):
+class SphericalGridParallel(SphericalGrid):
     '''
-    Object to register a given set of 3D coordinates as a spherical volume
+    SphericalGrid with parallelized computation of the (precomputed) volume moments.
+    '''
+    def voxel_moments_lm(self, voxels: np.ndarray, k_profile: np.ndarray) -> list[np.ndarray]:
+        '''
+        Compute the spherical harmonics for the given voxels in parallel.
+        Returns a list of multiplets, one (2l+1)-plet for each l.
+        '''
+        res = [[None]*(2*l+1) for l in range(self.l_max + 1)]
+
+        with ProcessPoolExecutor() as executor:
+            futures = {executor.submit(SphericalGrid.voxel_moment,self, l, m, voxels, k_prof): (l,m) 
+                       for l, k_prof in enumerate(k_profile) for m in range(-l, l + 1)}
+            for future in as_completed(futures):
+                l, m = futures[future]
+                res[l][m+l] = future.result()
+        return [np.stack(multiplet,axis=0) for multiplet in res]
+
+class Registrator(SphericalGridParallel):
+    '''
+    Object to register a given set of 3D coordinates as a spherical volume.
     '''
     def __init__(self, 
                  voxels: np.ndarray = None,
@@ -78,10 +103,11 @@ class Registrator(Spatial):
         self.k_profile = self.get_k_profile(grid_size, l_max, k_res)
         self.k_density = [np.exp(-self.k_profile[l] **2 / 2) for l in range(l_max + 1)]
         self.voxels = voxels
-        self.sl = list(self.Slm(voxels, self.k_profile)) if self.voxels is not None else []
-        self._gallery = (n_spherical, n_inplane,l_max)
-        self.preprocessed = None
 
+        # cached variables
+        self._gallery = (n_spherical, n_inplane,l_max)
+        self._preprocessed = None
+        self._volume_moments = list(self.voxel_moments_lm(voxels, self.k_profile)) if self.voxels is not None else []
         self._latest_correlations = None # temporary storage for the latest correlations
 
     @property
@@ -92,7 +118,7 @@ class Registrator(Spatial):
     @classmethod
     def get_k_profile(cls, grid_size: int, l_max: int, k_res: int) -> list[np.ndarray]:
         '''
-        Get the k-profile for the spherical harmonics. 
+        Get the k-profile for the radial grid, mapping to eigenvalues of the heat kernel.
         
         Pick k's that set the spherical Bessel functions to zero at the spherical grid boundary.
 
@@ -101,25 +127,15 @@ class Registrator(Spatial):
         r_max = (grid_size // 2)
         return list(jn_zeros(l_max, k_res * (grid_size // 2)) / r_max)
 
-    def set_reference(self, reference_coordinates: np.ndarray = None, reference_rotation: np.ndarray | tuple = np.eye(3), voxels: np.ndarray = None) -> Self:
+    def set_reference(self, voxels: np.ndarray) -> Self:
         '''
-        Set the reference coordinates and rotation for the registration
-        '''
-        try:
-            if voxels is not None:
-                self.voxels = voxels
-                return self
-            
-            assert reference_coordinates is not None, "Either voxels or reference_coordinates must be provided"
+        Set the reference coordinates and rotation for the registration.
 
-            if isinstance(reference_rotation, tuple):
-                reference_rotation = Rotation.from_euler('ZYZ', reference_rotation, degrees=True).as_matrix()
-            self.voxels = self.volume_map(reference_coordinates @ reference_rotation.T, 1.0)
-            
-            return self
-        finally:
-            if self.voxels is not None:
-                self.sl = self.Sl_parallel(self.k_profile)
+        This precomputes the volume moments for the given voxels.
+        '''
+        self.voxels = voxels
+        self._volume_moments = self.voxel_moments_lm(voxels, self.k_profile)
+        return self
     
     def relevant_k(self, thresh: float | int) -> list[tuple[int,int, int]]:
         '''
@@ -128,7 +144,7 @@ class Registrator(Spatial):
         if self.voxels is None:
             raise ValueError("No voxels set for filtering")
         idx = np.array([(l,m,i_k) for l,m in self.lm() for i_k, k in enumerate(self.k_profile[l])])
-        moments = np.concatenate([np.abs(s.flatten()) for s in self.sl])
+        moments = np.concatenate([np.abs(s.flatten()) for s in self._volume_moments])
         permutation = np.argsort(moments)[::-1]
         idx = idx[permutation]
         moments = moments[permutation]
@@ -148,54 +164,37 @@ class Registrator(Spatial):
         for l,_,i_k in relevant_modes:
             k_profile[l].append(self.k_profile[l][i_k])
             k_density[l].append(self.k_density[l][i_k])
-            sl[l].append(self.sl[l][:,i_k])
+            sl[l].append(self._volume_moments[l][:,i_k])
         self.k_profile = k_profile
         self.k_density = k_density
-        self.sl = [np.stack(ms_per_k,axis=-1) 
+
+        self._volume_moments = [np.stack(ms_per_k,axis=-1) 
                    if ms_per_k else np.zeros((2*l+1,len(self.k_profile[l]))) 
                    for l,ms_per_k in enumerate(sl)]
+        
         return self
     
     def preprocess(self) -> Self:
         '''
         Preprocess the voxel moments per rotation, inplace operation
         '''
-        self.preprocessed = [np.einsum('gmw,wk->gmk',
+        self._preprocessed = [np.einsum('gmw,wk->gmk',
                                        gallery,
-                                       sl) for gallery,sl in zip(self.gallery.matrices,self.sl)]
+                                       sl) for gallery,sl in zip(self.gallery.matrices,self._volume_moments)]
 
         return self
-    
-    def Slm_single(self, l:int, m:int, k_profile_l: np.ndarray) -> list[np.ndarray]:
-        return np.sum(self.fourier_bessel_expansion(l,m,k_profile_l) * self.voxels[...,None] * self.dV,axis=(0,1,2))                
-
-    def Sl_parallel(self, k_profile: np.ndarray) -> list[np.ndarray]:
-        '''
-        Compute the spherical harmonics for the given voxels in parallel.
-        Returns a list of multiplets, one for each l.
-        '''
-        res = [[None]*(2*l+1) for l in range(self.l_max + 1)]
-
-        with ProcessPoolExecutor() as executor:
-            futures = {executor.submit(Registrator.Slm_single,self, l, m, k_prof): (l,m) for l, k_prof in enumerate(k_profile) for m in range(-l, l + 1)}
-            for future in as_completed(futures):
-                l, m = futures[future]
-                res[l][m+l] = future.result()
-        return [np.stack(multiplet,axis=0) for multiplet in res]
 
     def correlations(self, coordinates: np.ndarray, sigma: float = 1.0) -> np.ndarray:
         '''
         Compute the correlations between the atom coordinate moments and the preprocessed volume moments
-
-        TODO The gaussian density factor / heat kernel eigenvalue could be included in the preprocessed moments.
         '''
-        if self.preprocessed is None:
+        if self._preprocessed is None:
             raise ValueError("Preprocessing not done, call preprocess() first")
-        correlation = np.sum([np.einsum('mk,gmk->g',Vlm,prep) for l, (Vlm, prep) in enumerate(zip(self.Vl(coordinates,sigma, self.k_profile, self.k_density),self.preprocessed)) if l > 0],axis=0)
+        correlation = np.sum([np.einsum('mk,gmk->g',Vlm,prep) for l, (Vlm, prep) in enumerate(zip(self.coordinate_moments_l(coordinates,sigma, self.k_profile, self.k_density),self._preprocessed)) if l > 0],axis=0)
         return correlation
 
     def align(self, coordinates: np.ndarray, sigma=1.0):
-
+        '''perform alignment of this grid and voxel-densities with the given coordinates'''
         self._latest_correlations = self.correlations(coordinates, sigma)
         best_fit = np.argmax(np.abs(self._latest_correlations),axis=0)
         return self.gallery[best_fit]
@@ -214,25 +213,4 @@ class Registrator(Spatial):
                             index = gallery_angles,
                             columns = labels)
 
-if __name__ == '__main__':
-    from tools.xmipp import AtomLine
-    from pathlib import Path
-    from tools.spider_files3 import open_volume
-    coordinates = np.stack([atom.coordinates for atom in AtomLine.from_pdb(Path(__file__).parent / "data" / "prepared2" / "approximation.pdb")])
-    voxels = open_volume(Path(__file__).parent / "data" / "prepared2" / 'reference.vol')
-
-    print("Preprocessing...")
-    t0 = time()
-    registrator = Registrator(n_spherical=8, n_inplane=8, l_max = 3)
-    registrator.set_reference(voxels = voxels)
-    registrator.filter_k(thresh=0.8).preprocess()
-    print(f"Preprocessing done in {time() - t0:.2f} seconds")
-
-    print("Aligning...")
-    t1 = time()
-    for rotation in registrator.align(coordinates, sigma=1.0).items():
-        print(rotation)
-    print(f"Alignment done in {time() - t1:.2f} seconds")
-
-    print(registrator.correlation_frame().to_string(index=True, float_format=lambda x: f"{x:.4f}"))
     
--- src/tools/sequences.py
+++ src/tools/sequences.py
@@ -3,8 +3,6 @@ Module providing generating sequences, to ensure terms always come in the same o
 '''
 from typing import Generator
 
-import numpy as np
-
 class ZernikeSequence:
     '''Parent class fixing the order of Zernike coefficients'''
     def __init__(self, l1: int, l2: int):
@@ -15,24 +13,22 @@ class ZernikeSequence:
             for n in range(l,self.l1 +1,2):
                 for m in range(-l, l+1):
                     yield l,n,m
+
     def ln(self) -> Generator[tuple[int,int],None,None]:
         for l in range(self.l2 + 1):
             for n in range(l,self.l1 +1,2):
                     yield l,n
 
 class SphericalSequence:
-    def __init__(self, l_max: int = 3, allow_isotropic: bool = True):
+    def __init__(self, l_max: int = 3):
         self.l_max = l_max
-        self.allow_isotropic = allow_isotropic
-
-    def start_l(self) -> int:
-        return 0 if self.allow_isotropic else 1
 
     def lm(self) -> Generator[tuple[int,int],None,None]:
-        for l in range(self.start_l(), self.l_max+1):
+        for l in range(self.l_max+1):
             for m in range(-l,l+1):
                 yield l,m
 
     def l(self) -> Generator[int,None,None]:
-        for l in range(self.start_l(), self.l_max+1):
+        for l in range(self.l_max+1):
             yield l
+
